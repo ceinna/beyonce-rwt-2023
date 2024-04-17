cdf3b5e8c10383a1ee2831520229b078be8af2ad
Ceinna <cdl451@nyu.edu>
2024-04-16 23:14:42 -0400

    formatting, comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-// Access Token
+// ACCESS TOKEN
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2Vpbm5hIiwiYSI6ImNsdWx1Nnl1ZTE1enkya28xc3gxeDE0eG8ifQ.2g1H9RrzSpRkOmNOns7Dpg'
 
 
-// BASE OF MAP: SPINNING GLOBE ZOOMED IN ON NORTHERN HEMISPHERE
-// ALL DATES PERFORMED IN EUROPE AND NORTH AMERICA
+// ---------------------------------------------------------------
+
+// ADD FUNCTIONALITY TO BASE MAP: SPINNING GLOBE ZOOMED IN ON NORTHERN HEMISPHERE
 
 var mapOptions = {
     container: 'map-container', // container ID
     style: 'mapbox://styles/mapbox/dark-v11', // dark basemap
-    center: [12.72369, 46.62962], // starting position [lng, lat]
-    zoom: 3.3, // starting zoom,
+    center: [1.39433, 48.04965], // starting position [lng, lat]
+    zoom: 3.8, // starting zoom,
     projection: 'globe',
     height: 3,
 }
@@ -31,6 +32,7 @@ const slowSpinZoom = 3;
 let userInteracting = false;
 const spinEnabled = true;
 
+// Function to rotate globe
 function spinGlobe() {
     const zoom = map.getZoom();
     if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
@@ -53,8 +55,26 @@ function spinGlobe() {
 map.on('mousedown', () => {
     userInteracting = true;
 });
-map.on('dragstart', () => {
-    userInteracting = true;
+
+// Restart spinning the globe when interaction is complete
+map.on('mouseup', () => {
+    userInteracting = false;
+    spinGlobe();
+});
+
+// These events account for cases where the mouse has moved
+// off the map, so 'mouseup' will not be fired.
+map.on('dragend', () => {
+    userInteracting = false;
+    spinGlobe();
+});
+map.on('pitchend', () => {
+    userInteracting = false;
+    spinGlobe();
+});
+map.on('rotateend', () => {
+    userInteracting = false;
+    spinGlobe();
 });
 
 // When animation is complete, start spinning if there is no ongoing interaction
@@ -62,92 +82,52 @@ map.on('moveend', () => {
     spinGlobe();
 });
 
+// When page is loaded, globe will spin as a default
 spinGlobe();
 
 
-// //  UPLOAD TWO IMAGES FOR TWO MAKERS AND HAVE THE MARKERS DISPLAYED BASED ON DIFFERENT CRITERIA (NUMBER OF NIGHTS PERFORMED)
+// ---------------------------------------------------------------
 
-// // Iterate over the beyonceData array and display image icons based on criteria
-// // Once the map is loaded, add the markers based on the data
-// map.on('load', function () {
-//     map.loadImage('https://i.postimg.cc/dDcq7dt5/silver-disco.png', function (error, image1) {
-//         if (error) throw error;
-//         map.addImage('silver-disco', image1); // 'silver-disco' is the image ID for icon 1
-//     });
-
-//     // Add custom image 2 as a map sprite
-//     map.loadImage('https://i.postimg.cc/jjXRLGyJ/gold-disco.png', function (error, image2) {
-//         if (error) throw error;
-//         map.addImage('gold-disco', image2); // 'gold-disco' is the image ID for icon 2
-//     });
-
-
-//     // Iterate over the beyonceData array
-//     beyonceData.forEach(tourDate => {
-//         // Determine the image icon based on the criteria
-//         let iconImage;
-//         if (tourDate.Nights === 1) {
-//             iconImage = 'silver-disco'; // Custom image icon 1
-//         } else {
-//             iconImage = 'gold-disco'; // Custom image icon 2
-//         }
-
-//         // Add a marker for each data point
-//         map.addLayer({
-//             id: tourDate["First Date"],
-//             type: 'symbol',
-//             source: {
-//                 type: 'geojson',
-//                 data: {
-//                     type: 'Feature',
-//                     geometry: {
-//                         type: 'Point',
-//                         coordinates: [tourDate.Longitude, tourDate.Latitude]
-//                     }
-//                 }
-//             },
-//             layout: {
-//                 'icon-image': iconImage,
-//                 'icon-size': 0.025
-//             }
-
-//         });
-//             map.on('click', tourDate["First Date"], function (e) {
-//                 new mapboxgl.Popup()
-//                     .setLngLat(e.features[0].geometry.coordinates)
-//                     .setText(`Beyoncé  first performed in ${tourDate.City}, ${tourDate.Country} on ${tourDate["First Date"]}. Recorded attendance for all nights performed was ${tourDate.Attendance} which earned her ${tourDate.Revenue} in revenue.`)
-//                     .addTo(map);
-
-//             });
-// });
-// });
+//  UPLOAD TWO IMAGES FOR TWO MAKERS AND HAVE THE MARKERS DISPLAYED BASED ON DIFFERENT CRITERIA (NUMBER OF NIGHTS PERFORMED)
 
 map.on('load', function () {
     beyonceData.forEach(tourDate => {
-      var imageUrl = tourDate.Nights > 1 ? 'gold-disco.png' : 'silver-disco.png';
-      var el = document.createElement('div');
-      el.className = 'marker';
-      el.style.backgroundImage = 'url(' + imageUrl + ')';
+        var imageUrl = tourDate.Nights > 1 ? 'gold-disco.png' : 'silver-disco.png';
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = 'url(' + imageUrl + ')';
 
-      new mapboxgl.Marker(el)
-        .setLngLat([tourDate.Longitude, tourDate.Latitude])
-        .setPopup(new mapboxgl.Popup().setHTML('Beyoncé  first performed in ' + tourDate.City +', '+ tourDate.Country + ' on ' + tourDate["First Date"] + '. Recorded attendance for all nights performed was ' + tourDate.Attendance + ' which earned her ' + tourDate.Revenue + ' in revenue.'))
-        .addTo(map);
+        new mapboxgl.Marker(el)
+            .setLngLat([tourDate.Longitude, tourDate.Latitude])
+            .setPopup(new mapboxgl.Popup().setHTML('Beyoncé  first performed in ' + tourDate.City + ', ' + tourDate.Country + ' on ' + tourDate["First Date"] + '. Recorded attendance for all nights performed was ' + tourDate.Attendance + ' which earned her ' + tourDate.Revenue + ' in revenue.'))
+            .addTo(map);
     });
 });
 
+
+// ---------------------------------------------------------------
+
+// ADD FUNCTIONALITY TO BUTTONS
+
+// For the button with a North America ID, when clicked will fly to center the United States
 document.getElementById('north-america').addEventListener('click', () => {
-    // Fly to a random location
-    map.flyTo({
-        center: [-104.60460, 40.09066],
-        essential: true // this animation is considered essential with respect to prefers-reduced-motion
-    });
+    userInteracting = true,
+        // Fly to a random location
+        map.flyTo({
+            center: [-107.20676, 40.09066],
+            zoom: 3.8,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+
 });
 
+// For the button with a European ID, when clicked will fly to center the Europe
 document.getElementById('europe').addEventListener('click', () => {
-    // Fly to a random location
-    map.flyTo({
-        center: [4.70531, 48.18800],
-        essential: true // this animation is considered essential with respect to prefers-reduced-motion
-    });
+    userInteracting = true,
+        // Fly to a random location
+        map.flyTo({
+            center: [6.46103, 49.92353],
+            zoom: 3.9,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 });
